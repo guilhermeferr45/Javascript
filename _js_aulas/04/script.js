e124b447eee8095f9ec1e9685088adbfa832a609
GUILHERME FERREIRA BEZERRA DA SILVA <guilherme.fbsilva3@senacsp.edu.br>
2023-10-16 22:23:08 -0300

    mes

--- _js_aulas/04/script.js
+++ _js_aulas/04/script.js
@@ -63,3 +63,48 @@ else if(num === 12){
 else{
     console.log("Mês não identificado");
 }
+
+//Escolha
+
+let numero = parseInt(prompt("Digite o número do mês"));
+
+switch (numero) {
+    case 1: 
+    console.log("Janeiro")
+    break;
+    case 2: 
+    console.log("Fevereiro")
+    break;
+    case 3: 
+    console.log("Março")
+    break;
+    case 4: 
+    console.log("Abril")
+    break;
+    case 5: 
+    console.log("Maio")
+    break;
+    case 6: 
+    console.log("Junho")
+    break;
+    case 7: 
+    console.log("Julho")
+    break;
+    case 8: 
+    console.log("Agosto")
+    break;
+    case 9: 
+    console.log("Setembro")
+    break;
+    case 10: 
+    console.log("Outubro")
+    break;
+    case 11: 
+    console.log("Novembro")
+    break;
+    case 12: 
+    console.log("Dezembro")
+    break;
+default:
+    console.log("Não identificado")
+}
